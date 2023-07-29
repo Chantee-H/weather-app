a50270989a8406c58b6eda6ed4e2319eaf2c9e47
Chantee Hoarau <chantee.hoarau123@gmail.com>
2023-07-29 09:37:15 +0200

    Wrap everything in a try catch for error handling

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ let feels = document.getElementById('feels');
 let humidity = document.getElementById('humidity');
 
 async function getWeather() {
+  try {
     const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2a33ec3bad3c4dc386f55016232007&q=${cityInput.value}`, {mode: 'cors'});
     const weatherData = await response.json();
     temp.innerHTML = weatherData.current.temp_c + '\u00B0C'; 
     condition.innerHTML = String(weatherData.current.condition.text);
     feels.innerHTML = 'Feels Like:' + ' ' + weatherData.current.feelslike_c + '\u00B0C';
     humidity.innerHTML = 'Humidty:' + ' ' + weatherData.current.humidity + '%';
+}
+ catch (error) {
+    alert(error.textContent = 'Error: city not found');
+}
 }
 
 form.onsubmit = e =>{
