56741bbf72caf6b1475f250fc858216ae0219370
Chantee Hoarau <chantee.hoarau123@gmail.com>
2023-07-29 09:31:42 +0200

    Fetch the weather api

--- script.js
+++ script.js
@@ -10,5 +10,5 @@ let feels = document.getElementById('feels');
 let humidity = document.getElementById('humidity');
 
 async function getWeather() {
-    
+    const response = fetch(`http://api.weatherapi.com/v1/current.json?key=2a33ec3bad3c4dc386f55016232007&q=${cityInput.value}`, {mode: 'cors'});
 }
