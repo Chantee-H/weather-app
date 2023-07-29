b50755c1ee80bc2f95c564e97aa470615f98ac8c
Chantee Hoarau <chantee.hoarau123@gmail.com>
2023-07-29 09:30:38 +0200

    Add async

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let form = document.getElementById('form');
+let cityInput = document.getElementById('cityInput');
+let output = document.getElementById('output');
+let cityName = document.getElementById('cityName');
+let temp = document.getElementById('temp');
+let condition = document.getElementById('condition');
+let feels = document.getElementById('feels');
+let humidity = document.getElementById('humidity');
+
+async function getWeather() {
+    
+}
