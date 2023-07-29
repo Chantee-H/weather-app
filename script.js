1b3446479552ea4f27137416bd38bdd16b348554
Chantee Hoarau <chantee.hoarau123@gmail.com>
2023-07-29 09:33:13 +0200

    Add await

--- script.js
+++ script.js
@@ -10,5 +10,10 @@ let feels = document.getElementById('feels');
 let humidity = document.getElementById('humidity');
 
 async function getWeather() {
-    const response = fetch(`http://api.weatherapi.com/v1/current.json?key=2a33ec3bad3c4dc386f55016232007&q=${cityInput.value}`, {mode: 'cors'});
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2a33ec3bad3c4dc386f55016232007&q=${cityInput.value}`, {mode: 'cors'});
+    const weatherData = await response.json();
+    temp.innerHTML = weatherData.current.temp_c + '\u00B0C'; 
+    condition.innerHTML = String(weatherData.current.condition.text);
+    feels.innerHTML = 'Feels Like:' + ' ' + weatherData.current.feelslike_c + '\u00B0C';
+    humidity.innerHTML = 'Humidty:' + ' ' + weatherData.current.humidity + '%';
 }
